aa06bce1e89985976d8beabee89216c18c6a6bbc
Justin Whitehead <justin.ray011@gmail.com>
2026-07-08 16:35:27 -0600

    Add Watch companion app target

---
+++ JourneyTracker Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JourneyTracker Watch App Watch App
+//
+//  Created by Justin Whitehead on 7/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JourneyTracker Watch App/JourneyTracker_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyTracker_Watch_AppApp.swift
+//  JourneyTracker Watch App Watch App
+//
+//  Created by Justin Whitehead on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct JourneyTracker_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
