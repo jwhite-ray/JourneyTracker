17b9befc46c32a151be16157f824c1410a39a6e4
Justin Whitehead <justin.ray011@gmail.com>
2026-07-08 16:25:29 -0600

    Initial Commit

---
+++ JourneyTracker/JourneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyTrackerApp.swift
+//  JourneyTracker
+//
+//  Created by Justin Whitehead on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct JourneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
